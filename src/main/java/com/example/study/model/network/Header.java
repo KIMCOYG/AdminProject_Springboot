a4942209ecbe97b4198f5a5868707779f397bc5a
KIMCOYG <kjhkjh9753@gmail.com>
2020-11-16 03:27:38 +0900

    userAPi read 완료

--- src/main/java/com/example/study/model/network/Header.java
+++ src/main/java/com/example/study/model/network/Header.java
@@ -47,7 +47,7 @@ public class Header<T> {
     }
 
     //ERROR
-    public static <T> Header<T> ERR(String description) {
+    public static <T> Header<T> ERROR(String description) {
         return (Header<T>) Header.builder()
                 .transactionTime(LocalDateTime.now())
                 .resultCode("ERROR")
